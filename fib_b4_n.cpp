c750a72d698ede1d73c1176a190725e34aafbdf7
ozielalves <ozielalves@ufrn.edu.br>
2018-02-23 14:07:19 -0300

    Update fib_b4_n.cpp

--- fib_b4_n.cpp
+++ fib_b4_n.cpp
@@ -1,3 +1,34 @@
-#include <iostrem>
+#include <iostream>
 
-std::cout
+int fibo ( int n )
+{
+    
+	if ( n == 1 or n ==2)
+	{
+	    
+	    return 1;
+	    
+	}else{
+	
+	return fibo( n - 1 ) + fibo( n - 2 );
+	
+	    
+	}
+}
+
+int main ( void )
+{
+	int n(0);
+	std::cout << "Informe o limite para o calculo da Sequencia de Fibonacci\n";
+	std::cin >> n;
+	std::cout << "Limite: f(" << n << ")" << std::endl;
+
+	for ( auto i(0) ; i < n ; i++ )
+	{
+        fibo(n);
+		std::cout << fibo(i+1) << std::endl;
+	}
+
+	return ( 0 );
+}
+

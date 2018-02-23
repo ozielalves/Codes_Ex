9b69ef76e698127bf09f7055218f7e377deefbd2
ozielalves <ozielalves@ufrn.edu.br>
2018-02-23 18:07:41 -0300

    Add files via upload

---
+++ troca_interna.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main ( void )
+{
+
+	int A[19];   
+	std::cout << "Informe os 20 numeros:\n";
+	auto i(0);
+
+	for ( i = 0 ; i <= 19 ; i++) // Entrada dos 20 numeros
+	{
+	
+	    std::cin >> A[r];
+	
+	}    
+
+	std::cout << "Os numeros são: \n";
+
+
+	for ( i = 0 ; i < 20 ; i++) // Saida dos 20 numeros
+	{
+	    
+	    std::cout << " " << A[i] << "  \n\n";
+
+	}
+w
+	return (0);
+
+}	

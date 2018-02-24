6ceb831a48a67eaa8a4553d03c90f5513c8ec692
ozielalves <ozielalves@ufrn.edu.br>
2018-02-24 10:29:57 -0300

    Update fib_b4_n.cpp

--- fib_b4_n.cpp
+++ fib_b4_n.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 
-int fibo ( int n )
-{
-    
-	if ( n == 1 or n ==2)
-	{
-	    
-	    return 1;
-	    
-	}else{
-	
-	return fibo( n - 1 ) + fibo( n - 2 );
-	
-	    
-	}
-}
-
 int main ( void )
 {
-	int n(0);
-	std::cout << "Informe o limite para o calculo da Sequencia de Fibonacci\n";
-	std::cin >> n;
-	std::cout << "Limite: f(" << n << ")" << std::endl;
 
-	for ( auto i(0) ; i < n ; i++ )
+	int A[19];   
+	int B[19];
+	std::cout << "Informe os 20 numeros:\n";
+// Indices para percorrer o vetor
+	auto i(0);
+    auto j(0);
+    
+	for ( i = 0 ; i < 20 ; i++ ) // Entrada dos 20 numeros
 	{
-        fibo(n);
-		std::cout << fibo(i+1) << std::endl;
+	
+	    std::cin >> A[i];
+	
+	}    
+
+	std::cout << "Os numeros são: \n";
+
+
+	for ( i = 0 ; i < 20 ; i++) // Saida dos 20 numeros
+	{
+	    
+	    std::cout << " " << A[i] << "  \n\n";
+
 	}
 
-	return ( 0 );
-}
+// Invertendo o Vetor A	
+	for ( i = 0, j = 19 ; i < 20 and j >=0 ; i++, j--)
+	{
+	    
+	    B[i] = A[j];
+	    
+	}
+	
+	std::cout << "O novo vetor e: \n";
 
+
+	for ( i = 0 ; i < 20 ; i++) // Saida do novo vetor
+	{
+	    
+	    std::cout << " " << B[i] << "  \n\n";
+
+	}
+	
+
+	return (0);
+
+}
